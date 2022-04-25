d6ae4ede51155bd0e09773ef68444661c694f83e
kunal pal varma <knlvarma8@gmail.com>
2022-04-25 06:08:33 -0400

    modified

--- Final_Files/nodes_ways_segments.py
+++ Final_Files/nodes_ways_segments.py
@@ -32,7 +32,7 @@ nodes_list = []
 
 q_nodes = "http://0.0.0.0/api/interpreter?data=[out:json];(node({},{},{},{});<;);out;".format(str(south),str(west),str(north),str(east))
 
-response_nodes = urlopen(,q_nodes)
+response_nodes = urlopen(q_nodes)
 result_nodes = json.loads(response_nodes.read())
 print("OSM data done")
 
