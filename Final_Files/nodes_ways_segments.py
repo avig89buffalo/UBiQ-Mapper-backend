e0f8657dfb066b5d9c0a1c225bfe109fb8037c99
kunal pal varma <knlvarma8@gmail.com>
2022-04-25 06:15:43 -0400

    modified

--- Final_Files/nodes_ways_segments.py
+++ Final_Files/nodes_ways_segments.py
@@ -33,7 +33,6 @@ http = urllib3.PoolManager()
 q_nodes = "http://0.0.0.0/api/interpreter?data=[out:json];(node({},{},{},{});<;);out;".format(str(south),str(west),str(north),str(east))
 
 response_nodes = http.request("GET",q_nodes)
-print(response_nodes)
 response_nodes = response_nodes.json()
 result_nodes = json.loads(response_nodes.read())
 print("OSM data done")
